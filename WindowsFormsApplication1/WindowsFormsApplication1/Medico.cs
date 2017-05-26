49d50ab28ecdb5ede05687f1df532d8a6371e972
simao <2160470@my.ipleiria.pt>
2017-05-26 11:55:13 +0100

    Medico Class

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Medico.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Medico
+    {
+        public string Nome { get; set; }
+
+
+        public string Especialidade;
+        public int _NIF;
+        public DateTime _inicio;
+        public DateTime _fim;
+
+        public int NIF
+        {
+            get { return _NIF; }
+            set
+            {
+                _NIF = value;
+                if (_NIF < 99999999)
+                {
+                    _NIF = 0;
+                }
+            }
+        }
+
+        public DateTime Inicio
+        {
+            get { return _inicio; }
+            set
+            {
+                _inicio = value;
+            }
+        }
+
+        public DateTime Fim
+        {
+            get { return _fim; }
+            set
+            {
+                _fim = value;
+            }
+        }
+
+    }
+    
+
+
+
+
+    }
+
