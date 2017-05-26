22bc2af52aba1f90edc92ba5162a96cd0e7361b3
Catarina Rodrigues <2160445@my.ipleiria.pt>
2017-05-26 12:35:29 +0100

    ins

--- WindowsFormsApplication1/WindowsFormsApplication1/Inserir_Medico.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Inserir_Medico.cs
@@ -12,16 +12,12 @@ namespace WindowsFormsApplication1
 {
     public partial class Inserir_Medico : Form
     {
+        private List<Medico> medicos;
+
         public Inserir_Medico()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Home Hfrm = new Home();
-            Hfrm.Show();
-            Close();
+            medicos = new List<Medico>();
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -29,6 +25,26 @@ namespace WindowsFormsApplication1
             Home Hfrm = new Home();
             Hfrm.Show();
             Close();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            Home Hfrm = new Home();
+            Hfrm.Show();
+            Close();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            Medico medico = new Medico
+            {
+                Nome = tbxNome.Text,
+                Especialidade = cbxEspecialidade.Text,
+                NIF = Convert.ToInt32(tbxNIF.Text),
+                Inicio = datetimeInicio.Value,
+                Fim = datetimeFim.Value
+            };
+            medicos.Add(medico);
         }
     }
 }
