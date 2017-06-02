13ccc664a3d7908d7d31cbf6ba05b618b81d0c2e
simao <2160470@my.ipleiria.pt>
2017-06-02 14:01:45 +0100

    waasda

---
+++ WindowsFormsApplication1/WindowsFormsApplication1Tests1/Marcar_consultaTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Tests
+{
+    [TestClass()]
+    public class Marcar_consultaTests
+    {
+        [TestMethod()]
+        public void Marcar_consultaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void carregarDadosTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RefreshCamposTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MarcarConsultaTest()
+        {
+            Consulta consulta = new Consulta { nome_paciente = "antonio", especialidade = "Psicologia", dia = Convert.ToDateTime("12/12/2016"), hora = Convert.ToDateTime("14:00:00"), MedicoId = 1 };
+
+            Assert.Fail();
+        }
+    }
+}
