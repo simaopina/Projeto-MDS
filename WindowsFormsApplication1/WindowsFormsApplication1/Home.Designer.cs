68c96394c1b3f63728f7f20ff4f06d9af052c1cc
sawak <sawak@DESKTOP-AE9MSO1>
2017-06-02 16:52:57 +0100

    hdzdfhnzdfc

--- WindowsFormsApplication1/WindowsFormsApplication1/Home.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Home.Designer.cs
@@ -46,11 +46,11 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.label1.Cursor = System.Windows.Forms.Cursors.No;
-            this.label1.Font = new System.Drawing.Font("Stormfaze", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(127, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(307, 42);
+            this.label1.Size = new System.Drawing.Size(259, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Clinica Cidadão";
             // 
@@ -100,7 +100,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Jéssica Gárcio";
+            this.label3.Text = "Jéssica Grácio";
             // 
             // label4
             // 
@@ -133,10 +133,10 @@
             // 
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
-            this.label10.Font = new System.Drawing.Font("square deal", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(474, -6);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(84, 64);
+            this.label10.Size = new System.Drawing.Size(124, 73);
             this.label10.TabIndex = 51;
             this.label10.Text = "CC";
             // 
